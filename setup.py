850a42a094a1f2b187f6ca493a4c18545fc443fa
Rob Dennis <rdennis@gmail.com>
2016-03-09 10:07:42 -0500

    bumping to 0.5.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ship_it',
-    version='0.4.0',
+    version='0.5.0',
     install_requires=['fabric', 'PyYaml', 'six', 'virtualenv'],
     packages=['ship_it'],
     url='github.com/robdennis/ship_it',
